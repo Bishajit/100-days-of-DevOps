e978c3ffb30c8b4dd56701bd4cf998ca70c383fc
Bishajit Lodh <Bishajitklodh@gmail.com>
2022-04-10 17:53:46 -0400

    new files

---
+++ 100_days_of_DevOps-day_1/sg.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "sg-for-ec2-for-100days-day1" {
+  name        = "sg-for-ec2-for-100days-day1"
+  description = "Allow 443 and (Https/port 22)"
+  vpc_id      = default
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
